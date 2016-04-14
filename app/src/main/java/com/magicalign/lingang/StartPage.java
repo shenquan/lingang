061854c7bd31ebfe756671c9c528b4cebdaf1bba
Han shenquan <1173956412@qq.com>
2016-04-14 19:13:37 +0800

    基本完成-未添加启动页

---
+++ app/src/main/java/com/magicalign/lingang/StartPage.java
@@ -0,0 +1,14 @@
+package com.magicalign.lingang;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/4/14.
+ */
+public class StartPage extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
