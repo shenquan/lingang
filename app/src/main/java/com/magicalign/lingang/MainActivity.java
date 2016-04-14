b5833a85e9d3abba46cd88a930d2c15493ad5b0f
Han shenquan <1173956412@qq.com>
2016-04-14 19:16:44 +0800

    v-1

--- app/src/main/java/com/magicalign/lingang/MainActivity.java
+++ app/src/main/java/com/magicalign/lingang/MainActivity.java
@@ -1,13 +1,46 @@
 package com.magicalign.lingang;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getActionBar().hide();
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("file:///android_asset/1_0ShouYe.html");
+        //允许js
+        webView.getSettings().setJavaScriptEnabled(true);
+        //点击页面设置重写
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+
+
+    }
+
+    //重写按下返回键的效果
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (webView.canGoBack() && keyCode == KeyEvent.KEYCODE_BACK) {
+            webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
